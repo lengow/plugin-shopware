66741a395b3eebf4b4e5149009d6b64921e2df27
Nicolas Maugendre <nicolas.maugendre@lengow.com>
2016-07-07 11:26:30 +0200

    Enable search function in export grid

--- Views/backend/lengow/store/article.js
+++ Views/backend/lengow/store/article.js
@@ -7,7 +7,21 @@ Ext.define('Shopware.apps.Lengow.store.Article', {
      // Define how much rows loaded with one request
     pageSize: 40,
 
-    defaultRootId: 1,
+    /**
+     * Enable remote filtering
+     */
+    remoteFilter: true,
+
+    /**
+     * Enable remote sorting
+     */
+    remoteSort: true,
+
+    /**
+     * Auto load the store after the component is initialized
+     * @boolean
+     */
+    autoLoad: false,
 
     configure: function() {
         return { controller: 'LengowExport' };
