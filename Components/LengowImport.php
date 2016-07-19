4cc8740ea09003983059ac94d5f35bf0b36587bd
Nicolas Maugendre <nicolas.maugendre@lengow.com>
2016-07-19 09:42:00 +0200

    Update translations

---
+++ Components/LengowImport.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Copyright 2016 Lengow SAS.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * @author    Team Connector <team-connector@lengow.com>
+ * @copyright 2016 Lengow SAS
+ * @license   http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+/**
+* Lengow Import Class
+*/
+
+class Shopware_Plugins_Backend_Lengow_Components_LengowImport
+{
+    /**
+     * @var array valid states lengow to create a Lengow order
+     */
+    public static $LENGOW_STATES = array(
+        'accepted',
+        'waiting_shipment',
+        'shipped',
+        'closed'
+    );
+
+    /**
+     * Construct the import manager
+     *
+     * @param array params optional options
+     * string    $marketplace_sku    lengow marketplace order id to import
+     * string    $marketplace_name   lengow marketplace name to import
+     * integer   $shop_id            Id shop for current import
+     * boolean   $force_product      force import of products
+     * boolean   $preprod_mode       preprod mode
+     * string    $date_from          starting import date
+     * string    $date_to            ending import date
+     * integer   $limit              number of orders to import
+     * boolean   $log_output         display log messages
+     */
+    public function __construct($params = array())
+    {
+    	
+    }
+}
