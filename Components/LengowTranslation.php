500f3093dc93753c02e848c1caefb9577263d198
benjamin <benjamin.le-neve@lengow>
2016-07-20 14:33:58 +0200

    Change for PSR-2

--- Components/LengowTranslation.php
+++ Components/LengowTranslation.php
@@ -1,5 +1,24 @@
 <?php
 
+/**
+ * Copyright 2016 Lengow SAS.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * @author    Team Connector <team-connector@lengow.com>
+ * @copyright 2016 Lengow SAS
+ * @license   http://www.apache.org/licenses/LICENSE-2.0
+ */
 class Shopware_Plugins_Backend_Lengow_Components_LengowTranslation
 {
     /**
@@ -12,13 +31,22 @@ class Shopware_Plugins_Backend_Lengow_Components_LengowTranslation
      */
     public $fallbackIsoCode = 'default';
 
-	public function t($message, $args = array(), $iso_code)
-	{
-        if (!isset(self::$translation[$iso_code])) {
+    /**
+     * Translate message
+     *
+     * @param string $message
+     * @param array  $args
+     * @param string $isoCode
+     *
+     * @return string Final Translate string
+     */
+    public function t($message, $args = array(), $isoCode = null)
+    {
+        if (!isset(self::$translation[$isoCode])) {
             $this->loadFile();
         }
-        if (isset(self::$translation[$iso_code][$message])) {
-            return $this->translateFinal(self::$translation[$iso_code][$message], $args);
+        if (isset(self::$translation[$isoCode][$message])) {
+            return $this->translateFinal(self::$translation[$isoCode][$message], $args);
         } else {
             if (!isset(self::$translation[$this->fallbackIsoCode])) {
                 $this->loadFile($this->fallbackIsoCode);
@@ -29,7 +57,7 @@ class Shopware_Plugins_Backend_Lengow_Components_LengowTranslation
                 return 'Missing Translation ['.$message.']';
             }
         }
-	}
+    }
 
     /**
      * Translate string
@@ -57,38 +85,45 @@ class Shopware_Plugins_Backend_Lengow_Components_LengowTranslation
     /**
      * Load ini file
      *
-     * @param string $iso_code
-     * @param string $filename file location
+     * @param string $fileName file location
+     * @param string $isoCode
      *
      * @return boolean
      */
     public function loadFile($fileName = null, $isoCode = null)
     {
         if (!$fileName) {
-        	$pluginPath = Shopware_Plugins_Backend_Lengow_Components_LengowMain::getLengowFolder();
-            $fileName = $pluginPath . 'Snippets/backend/Lengow/translation.ini';
+            $pluginPath = Shopware_Plugins_Backend_Lengow_Components_LengowMain::getLengowFolder();
+            $fileName = $pluginPath.'Snippets/backend/Lengow/translation.ini';
         }
         $translation = array();
         if (file_exists($fileName)) {
-        	try {
-    			self::$translation = parse_ini_file($fileName, true);
-    		} catch (Exception $e) {
-	            throw new Shopware_Plugins_Backend_Lengow_Components_LengowException(
-	                Shopware_Plugins_Backend_Lengow_Components_LengowMain::setLogMessage('Translation file could not be load : ' . $e)
-	            );
-    		}
+            try {
+                self::$translation = parse_ini_file($fileName, true);
+            } catch (Exception $e) {
+                throw new Shopware_Plugins_Backend_Lengow_Components_LengowException(
+                    Shopware_Plugins_Backend_Lengow_Components_LengowMain::setLogMessage(
+                        'Translation file could not be load : '.$e
+                    )
+                );
+            }
         }
         self::$translation[$isoCode] = $translation;
         return count($translation) > 0;
     }
 
+    /**
+     * File contains Iso code
+     *
+     * @param string $isoCode
+     *
+     * @return boolean
+     */
     public static function containsIso($isoCode)
     {
-        if (!isset(self::$translation[$iso_code])) {
+        if (!isset(self::$translation[$isoCode])) {
             $this->loadFile();
         }
-
         return array_key_exists($isoCode, self::$translation);
     }
-
 }
