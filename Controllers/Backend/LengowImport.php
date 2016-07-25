9197f2d6e23a35e33b90d12ae6f3126177f18e53
Nicolas Maugendre <nicolas.maugendre@lengow.com>
2016-07-25 18:11:23 +0200

    Add import frontend  controller

---
+++ Controllers/Backend/LengowImport.php
@@ -0,0 +1,107 @@
+<?php
+
+use Doctrine\ORM\Query\Expr;
+
+/**
+ * Copyright 2016 Lengow SAS.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * @author    Team Connector <team-connector@lengow.com>
+ * @copyright 2016 Lengow SAS
+ * @license   http://www.apache.org/licenses/LICENSE-2.0
+ */
+class Shopware_Controllers_Backend_LengowImport extends Shopware_Controllers_Backend_ExtJs
+{
+    /**
+     * Check if Lengow import setting is enabled
+     */
+    public function getImportSettingStatusAction()
+    {
+        $status = Shopware_Plugins_Backend_Lengow_Components_LengowConfiguration::getConfig('lengowEnableImport');
+        $this->View()->assign(array(
+            'success' => true,
+            'data'    => $status
+        ));
+    }
+
+    /**
+     * Get translations and create labels displayed in import panel
+     * Used despite Shopware translation tool because of parameters which are not settable
+     */
+    public function getPanelContentsAction()
+    {
+        $locale = Shopware_Plugins_Backend_Lengow_Components_LengowMain::getLocale();
+        $nbDays = Shopware_Plugins_Backend_Lengow_Components_LengowConfiguration::getConfig('lengowImportDays');
+        $data['importDescription'] = Shopware_Plugins_Backend_Lengow_Components_LengowMain::decodeLogMessage(
+            'order/panel/description',
+            $locale,
+            array(
+                'nb_days' => $nbDays
+            )
+        );
+        // Get last import date
+        $lastImport = Shopware_Plugins_Backend_Lengow_Components_LengowImport::getLastImport();
+        $data['lastImport'] = Shopware_Plugins_Backend_Lengow_Components_LengowMain::decodeLogMessage(
+            'order/panel/last_import',
+            $locale,
+            array(
+                'import_date' => $lastImport
+            )
+        );
+        $this->View()->assign(array(
+            'success' => true,
+            'data'    => $data
+        ));
+    }
+
+    /**
+     * Execute import process and get result/errors
+     */
+    public function launchImportProcessAction()
+    {
+        $import = new Shopware_Plugins_Backend_Lengow_Components_LengowImport();
+        $locale = Shopware_Plugins_Backend_Lengow_Components_LengowMain::getLocale();
+        $result = $import->exec();
+        $data = array();
+        $success = !empty($result['error']) ? false : true; // If error during import process
+        foreach ($result as $key => $nbOrders) {
+            if ($key == 'error') {
+                continue;
+            }
+            $data[$key] = Shopware_Plugins_Backend_Lengow_Components_LengowMain::decodeLogMessage(
+                'order/panel/' . $key,
+                $locale,
+                array(
+                    'nb_order' => $nbOrders
+                )
+            );
+        }
+        if (!$success) {
+            // Retrieve log of the day
+            $logUrl = Shopware_Plugins_Backend_Lengow_Components_LengowMain::getPathPlugin() .
+                'Logs/logs-'.date('Y-m-d').'.txt';
+            $data['error'] = Shopware_Plugins_Backend_Lengow_Components_LengowMain::decodeLogMessage(
+                'order/panel/order_import_failed',
+                $locale,
+                array(
+                    'log_url' => $logUrl
+                )
+            );
+        }
+        $this->View()->assign(array(
+            'success' => $success,
+            'data'    => $data
+        ));
+    }
+}
--- Views/backend/lengow/controller/main.js
+++ Views/backend/lengow/controller/main.js
@@ -11,8 +11,27 @@ Ext.define('Shopware.apps.Lengow.controller.Main', {
             logStore: Ext.create('Shopware.apps.Lengow.store.Logs')
         }).show();
 
+        me.initImportTab();
+
         me.mainWindow.maximize();
         me.callParent(arguments);
+    },
+
+    /**
+     * Hide import tab if import setting option is not enabled
+     */
+    initImportTab: function() {
+        Ext.Ajax.request({
+            url: '{url controller="LengowImport" action="getImportSettingStatus"}',
+            method: 'POST',
+            type: 'json',
+            success: function(response) {
+                var status = Ext.decode(response.responseText)['data'];
+                if (!status) {
+                    Ext.getCmp('lengowTabPanel').child('#lengowImportTab').tab.hide();
+                }
+            }
+        });
     }
 });
 //{/block}
