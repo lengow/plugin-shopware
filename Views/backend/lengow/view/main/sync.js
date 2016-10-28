276a928d047beecde9f3b0d3402387265b734b6f
Benjamin Le Nevé <benjamin.le-neve@lengow.com>
2016-10-28 15:49:09 +0200

    Add rec environment in connector and home.js

--- Views/backend/lengow/view/main/sync.js
+++ Views/backend/lengow/view/main/sync.js
@@ -25,11 +25,13 @@ Ext.define('Shopware.apps.Lengow.view.main.Sync', {
             if (me.syncLink) {
                 // me.url = 'http://cms.lengow.io/sync/';
                 // me.url = 'http://cms.lengow.net/sync/';
-                me.url = 'http://cms.lengow.dev/sync/';
+                me.url = 'http://cms.lengow.rec/sync/';
+                // me.url = 'http://cms.lengow.dev/sync/';
             } else {
                 // me.url = 'http://cms.lengow.io/';
                 // me.url = 'http://cms.lengow.net/';
-                me.url = 'http://cms.lengow.dev/';
+                me.url = 'http://cms.lengow.rec/';
+                // me.url = 'http://cms.lengow.dev/';
             }
             sync_iframe.src = me.url;
             sync_iframe.onload = function() {
