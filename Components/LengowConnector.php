562b48e7a2427e2b919aebba2fd38bd77711951d
Benjamin Le Nevé <benjamin.le-neve@lengow.com>
2016-12-13 11:47:48 +0100

    Change Curl constants

--- Components/LengowConnector.php
+++ Components/LengowConnector.php
@@ -45,7 +45,7 @@ class Shopware_Plugins_Backend_Lengow_Components_LengowConnector
     public static $curlOpts = array (
         CURLOPT_CONNECTTIMEOUT => 10,
         CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_TIMEOUT        => 20,
+        CURLOPT_TIMEOUT        => 10,
         CURLOPT_USERAGENT      => 'lengow-php-sdk',
     );
 
@@ -80,9 +80,9 @@ class Shopware_Plugins_Backend_Lengow_Components_LengowConnector
     protected $lengowUrls = array (
         '/v3.0/orders'        => 15,
         '/v3.0/marketplaces'  => 10, 
-        '/v3.0/subscriptions' => 5,
+        '/v3.0/subscriptions' => 3,
         '/v3.0/stats'         => 3,
-        '/v3.0/cms'           => 5,
+        '/v3.0/cms'           => 3,
     );
 
     /**
