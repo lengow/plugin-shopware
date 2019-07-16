ba054d87b2766afea4313dbeeec35800ebdbacf6
Benjamin Le Nevé <benjamin.le-neve@lengow.com>
2019-07-16 11:56:49 +0200

    Delete use Shop

--- Components/LengowConfiguration.php
+++ Components/LengowConfiguration.php
@@ -28,8 +28,6 @@
  * @license     https://www.gnu.org/licenses/agpl-3.0 GNU Affero General Public License, version 3
  */
 
-use Shopware\Models\Shop\Shop;
-
 /**
  * Lengow Configuration Class
  */
